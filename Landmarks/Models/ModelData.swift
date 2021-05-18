e97bb15ffe62e0ab23db128720a2c39bc9a98abb
016270 <yanglong@htsc.com>
2021-05-18 16:26:43 +0800

    丰富页面组织结构

--- Landmarks/Models/ModelData.swift
+++ Landmarks/Models/ModelData.swift
@@ -12,6 +12,17 @@ final class ModelData: ObservableObject {
     // An observable object needs to publish any changes to its data, so that its subscribers can pick up the change.
     @Published var landmarks: [Landmark] = load("landmarkData.json")
     var hikes: [Hike] = load("hikeData.json")
+    
+    var features: [Landmark] {
+        landmarks.filter { $0.isFeatured }
+    }
+    
+    var categories: [String: [Landmark]] {
+        Dictionary(
+            grouping: landmarks,
+            by: { $0.category.rawValue }
+        )
+    }
 }
 
 func load<T: Decodable>(_ filename: String) -> T {
